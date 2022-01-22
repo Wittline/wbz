613c87b987055fd01273f51dd8da6eecfc207ab1
Wittline <8701464+Wittline@users.noreply.github.com>
2022-01-21 22:17:30 -0600

    Update bwt.py

--- code/bwt.py
+++ code/bwt.py
@@ -1,64 +1,38 @@
-class Bwt:
+from operator import itemgetter
 
-    def __init__(self, sc, fc):
-        # space character
-        self.sc = sc
+class Bwt(object):
+
+    def __init__(self, fc):
         # mark character
         self.fc = fc
-        
 
-    def __fix_str(self, str):
-        str = str.replace(' ', self.sc)
-        return str + self.fc
+    def encode(self,s):
+        n = len(s)
+        m = sorted([s[i:n]+s[0:i] for i in range(n)])
+        I = m.index(s)
+        L = [q[-1] for q in m]
+        L = L[:]
+        L[I:I] = [self.fc]
+        return L
 
-    def __sa(self, s):
-        st = sorted([(s[x:], x) for x in range(0, len(s))])
-        return map(lambda x: x[1], st)
 
-    def __rank(self, bw):
-        ts = dict()
-        rks = []
-        for c in bw:
-            if c not in ts:
-                ts[c] = 0
-            rks.append(ts[c])
-            ts[c] += 1
-        return rks, ts
+    def decode(self, s):
 
-    def __fc(self, ts):
-        first = {}
-        tc = 0
-        for c, count in sorted(ts.items()):
-            first[c] = (tc, tc + count)
-            tc += count
-        return first      
+        I = s.index(self.fc)
+        s.pop(I)
 
-    def encode(self, str):
+        n = len(s)
+        X = sorted([(i, x) for i, x in enumerate(s)], key=itemgetter(1))
 
-        str = self.__fix_str(str)
+        T = [None] * n
+        for i, y in enumerate(X):
+            j, _ = y
+            T[j] = i
 
-        bw = []
-        for i in self.__sa(str):
-            if i == 0:
-                bw.append(self.fc)
-            else:
-                bw.append(str[i-1])
+        Tx = [I]
+        for i in range(1, n):
+            Tx.append(T[Tx[i-1]])
 
-        outstr = ''.join(bw)
-
-        print(str, "<------->", outstr)
-
-        return outstr
-    
-    def decode(self, bw):
-        rks, ts = self.__rank(bw)
-        first = self.__fc(ts)
-        ri = 0
-        t = self.fc
-        while bw[ri] != self.fc:
-            c = bw[ri]
-            t = c + t            
-            ri = first[c][0] + rks[ri]
-
-        t = t.replace(self.sc, ' ')
-        return t[0:-1]
+        S = [s[i] for i in Tx]
+        S.reverse()
+        return S
