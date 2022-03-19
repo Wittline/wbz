406baf5725c8caa8f1728103c626d7cb9e74599a
Wittline <8701464+Wittline@users.noreply.github.com>
2022-03-18 19:11:06 -0600

    Update huffman.py

--- code/huffman.py
+++ code/huffman.py
@@ -17,12 +17,9 @@ class Huffman:
         self.compressedFile = None
         self.tf = {}
 
-    def encode(self, data):
+    def encode(self, data):    
 
-        c = dict(Counter(data))
-            
-        for k, v in c.items():
-                self.tf[k] = v
+        self.tf = dict(Counter(data))
 
         lk  = self.tf.keys()      
 
@@ -38,12 +35,13 @@ class Huffman:
         self._huffmanCodes(tl.pop(0))
 
         self.compressedFile = ''.join([self.huffcodes.get(b) for b in data])
+        
 
         return self.compressedFile
 
 
     def decode(self, datac, ht, lengths):
-        o_file = []
+        datad = []
         c_size = len(datac)     
 
         index = 0
@@ -51,11 +49,99 @@ class Huffman:
             for l in lengths:
                 possible_code = datac[index: index + l]
                 if possible_code in ht.keys():
-                    o_file.append(int(ht[possible_code])) 
+                    datad.append(ht[possible_code])
                     index = index + l
-                    break                
+                    break
                 
-        return o_file
+        return datad
+    
+    
+    def __data_huffcodes(self):
+        return [(k, v, len(v)) for k, v in self.huffcodes.items()]
+    
+    
+    def __get_diffs_lengths(self, lengths): 
+        return [ lengths[i+1] - lengths[i] for i in range(len(lengths) - 1)]
+
+    
+    def sorted_lengths_by_frequency(self):
+
+        lengths = list(set([len(v) for v in self.huffcodes.values()]))
+        lengths.sort(reverse = False)
+        return lengths
+
+
+    def encode_huffcodes(self):
+
+        e_table = ''
+
+        #max length symbol
+        lms = max(self.huffcodes.keys())
+        blms = len(bin(lms)) - 2
+
+        #min length codes
+        lengths = list(set([len(v) for v in self.huffcodes.values()]))
+        lengths.sort(reverse = False)
+        lmc = min(lengths)
+        blmc = len(bin(lmc)) - 2
+
+        #max length of lengths differences
+        diffs = self.__get_diffs_lengths(lengths)
+        lmd = max(diffs)
+        blmd = len(bin(lmd)) - 2
+        
+        data_huffcodes = self.__data_huffcodes()
+
+        dhc = sorted(data_huffcodes, key=lambda t: t[2])
+
+        sym = format(dhc[0][0], "0" + str(blms) + "b")
+        lc = format(lmc, "0" + str(blmc) + "b")
+        c = dhc[0][1]
+
+        e_table += sym + lc + c
+
+        last_len = dhc[0][2]
+
+
+        for i in range(1, len(dhc)):
+            sym = format(dhc[i][0], "0" + str(blms) + "b")
+            if dhc[i][2] > last_len:
+                lc = format(dhc[i][2] - last_len, "0" + str(blmd) + "b")
+                last_len = dhc[i][2]
+            else:
+                lc = format(0, "0" + str(blmd) + "b")
+            c =  dhc[i][1]
+            e_table += sym + lc + c
+        
+
+        header = format(blms, "08b")
+        header += format(blmc, "08b")
+        header += format(blmd, "08b")
+        header += format(len(dhc), "08b")    
+
+        return header + e_table
+
+
+    def decode_huffcodes(self, datac):
+
+        header = datac[0:32]
+        datac = datac[32:]
+        lms = datac[0:8]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     # def __get_metadata(self, datac):
 
@@ -73,5 +159,5 @@ class Huffman:
             self.codes.append("1")
             self._huffmanCodes(r)
             self.codes.pop()
-        else:
+        else:            
             self.huffcodes[tl.value] = ''.join(self.codes)
