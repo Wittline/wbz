fb136889a8843a3e8f329a38f5078a7d73ca16cc
Wittline <8701464+Wittline@users.noreply.github.com>
2022-03-27 22:49:20 -0600

    Updating

--- code/bzip2.py
+++ code/bzip2.py
@@ -11,33 +11,34 @@ from bitsbytes import BitsBytes
 
 class bzip2:
 
-    def __init__(self, chunk_size):
-        self.chunk_size = chunk_size
+    def __init__(self, chunk_size, sc):
+        self.chunk_size = chunk_size        
+        self.bwt = BWT(sc)
+        self.mtf = MTF()
+        self.tb = BitsBytes()
+        self.huf = Huffman()
 
     def encode(self, seq):
-
-        bwt, mtf, tb, huff = BWT(';'), MTF(), BitsBytes(), Huffman()
+        
         prl = Parallel(True)
 
-        bwt_mtf = prl.parallel(seq, self.chunk_size, [bwt, mtf])
+        bwt_mtf = prl.parallel(seq, self.chunk_size, [self.bwt, self.mtf])
 
-        datac = huff.encode(bwt_mtf) 
+        datac = self.huf.encode(bwt_mtf) 
 
         size = ((len(datac) // 8) // prl.cpus) * 8
 
-        cdata = prl.parallel(datac, size, [tb])
+        cdata = prl.parallel(datac, size, [self.tb])
         
         return bytearray(cdata)
         
     def decode(self, seq):
 
-        bwt, mtf, tb, huff = BWT(';'), MTF(), BitsBytes(), Huffman()
         prl = Parallel(False)
-
         size = (len(seq) // prl.cpus)        
-        datac = prl.parallel(seq, size, [tb])        
-        datad = huff.decode(''.join(sb for sb in datac))
-        data = prl.parallel(datad, self.chunk_size + 1, [mtf,bwt])
+        datac = prl.parallel(seq, size, [self.tb])        
+        datad = self.huf.decode(''.join(sb for sb in datac))
+        data = prl.parallel(datad, self.chunk_size + 1, [self.mtf, self.bwt])
                 
         return bytearray(data)
 
@@ -48,7 +49,7 @@ if __name__ == '__main__':
     pathfilecom = 'data/data_compressed.txt'
     pathfileun = 'data/data_uncompressed.txt'
     fh = FileHandler()
-    bzip = bzip2(30000)    
+    bzip = bzip2(50000, ';')
 
     inicio = tiempo.default_timer()
 
